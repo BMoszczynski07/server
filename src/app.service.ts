2706eacb9e6ed59b0b53e02af24b34b798075072
Bartosz Moszczyński <109615293+BMoszczynski07@users.noreply.github.com>
2023-06-02 23:52:43 +0200

    test commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore';
   }
 }
